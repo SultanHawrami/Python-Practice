eae9c4e8ea3a6149169b8fa837d29b50ab8c348d
Sultan Hawrami <ravendevran@gmail.com>
2026-03-04 22:19:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# variable = a container for a value
+
+#Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+Spongebob = Patrick = Sandy = Squidward = 30
+print (Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
